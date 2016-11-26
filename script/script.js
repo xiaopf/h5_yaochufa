588aa7669fa7b140068a269ad5472d49f0957d79
xiaopf <326258842@qq.com>
2016-11-27 00:11:19 +0800

    reig

--- script/script.js
+++ script/script.js
@@ -56,7 +56,8 @@ $pages.swipeDown(function(){
 
 
 
-<<<<<<< HEAD
+
+
 var $body=$('body').get(0);
 
 
@@ -95,22 +96,6 @@ $body.addEventListener("touchmove", function(event) {
  if (i !== "11") {
 
 
-=======
-var n=$('body').get(0);
-var f;
-n.addEventListener("touchstart", function(e) {         //n为h5页面的body
-                f = e.touches[0].clientY
-},!1),
-n.addEventListener("touchmove", function(t) {
-  var i = "11"
-  , n = this;
-  if (0 === n.scrollTop)                                  //滚动条最顶部的情况。
-       // 如果说没有滚动条的情况，i = "00"; 有滚动条的情况为“01”。
-       i = n.offsetHeight === n.scrollHeight ? "00" : "01";     
-  else if (n.scrollTop + n.offsetHeight >= n.scrollHeight)//有滚动条，滚动到最底部的情况。
-       i = "10";                 
- if ("11" !== i) {
->>>>>>> origin/gh-pages
        //01是向上滑动或者滑不动，10是向下滑动。
 
 
