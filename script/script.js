0fa55231425bb62a4328cae99c04a741659795dc
xiaopf <326258842@qq.com>
2016-11-27 01:24:20 +0800

    125

--- script/script.js
+++ script/script.js
@@ -12,47 +12,47 @@ $('#page1').show();
 var first_page=$pages.get(0);
 var last_page=$pages.get($pages.length-1); //数字表示一共页数的长度-1
 
-$pages.swipeUp(function(){
-	$(this).addClass('up_fade_out');	
-    if ($(this).get(0)==last_page) {
-    	$(first_page).addClass('up_top').show().addClass('up_fade_in');
-	}else{
-        $(this).next().addClass('up_top').show().addClass('up_fade_in');
-	}
-    var $that=$(this);
-	setTimeout(function(){
+// $pages.swipeUp(function(){
+// 	$(this).addClass('up_fade_out');	
+//     if ($(this).get(0)==last_page) {
+//     	$(first_page).addClass('up_top').show().addClass('up_fade_in');
+// 	}else{
+//         $(this).next().addClass('up_top').show().addClass('up_fade_in');
+// 	}
+//     var $that=$(this);
+// 	setTimeout(function(){
         
-		$that.removeClass('up_fade_out');
-        $pages.hide();
-		if ($that.get(0)==last_page) {
-	       $(first_page).removeClass('up_top').removeClass('up_fade_in').show();
-		}else{
-		   $that.next().removeClass('up_top').removeClass('up_fade_in').show();	
-		}
-	},400)
+// 		$that.removeClass('up_fade_out');
+//         $pages.hide();
+// 		if ($that.get(0)==last_page) {
+// 	       $(first_page).removeClass('up_top').removeClass('up_fade_in').show();
+// 		}else{
+// 		   $that.next().removeClass('up_top').removeClass('up_fade_in').show();	
+// 		}
+// 	},400)
 	
-})
+// })
 
 
-$pages.swipeDown(function(){
-	$(this).addClass('down_fade_out');	
-	if ($(this).get(0)==first_page) {
-    	$(last_page).addClass('down_top').show().addClass('down_fade_in');
-	}else{
-        $(this).prev().addClass('down_top').show().addClass('down_fade_in');
-	}
-    var $that=$(this);
-	setTimeout(function(){
-		$that.removeClass('down_fade_out');
-        $pages.hide();
-		if ($that.get(0)==first_page) {
-	       $(last_page).removeClass('down_top').removeClass('down_fade_in').show();
-		}else{
-		   $that.prev().removeClass('down_top').removeClass('down_fade_in').show();
-		}
-	},400)
+// $pages.swipeDown(function(){
+// 	$(this).addClass('down_fade_out');	
+// 	if ($(this).get(0)==first_page) {
+//     	$(last_page).addClass('down_top').show().addClass('down_fade_in');
+// 	}else{
+//         $(this).prev().addClass('down_top').show().addClass('down_fade_in');
+// 	}
+//     var $that=$(this);
+// 	setTimeout(function(){
+// 		$that.removeClass('down_fade_out');
+//         $pages.hide();
+// 		if ($that.get(0)==first_page) {
+// 	       $(last_page).removeClass('down_top').removeClass('down_fade_in').show();
+// 		}else{
+// 		   $that.prev().removeClass('down_top').removeClass('down_fade_in').show();
+// 		}
+// 	},400)
 
-})
+// })
 
 
 
@@ -125,7 +125,7 @@ n.addEventListener("touchmove", function(t) {
  //       var r = t.touches[0].clientY - f > 0 ? "10" : "01";
  //       if (!(parseInt(i, 2) & parseInt(r, 2)))
  //                        //e(t)
-
+   alert(n.scrollTop)
           if(n.scrollTop!=0){
           　t.stopPropagation();
 　　　　　　t.preventDefault();
