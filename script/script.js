f468d27e62f76115ff87b2f3ee0f68149cdbe8a2
xiaopf <326258842@qq.com>
2016-11-27 12:00:14 +0800

    try

--- script/script.js
+++ script/script.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var $pages=$('#viewport .pages');
+var $viewport=$('#viewport');
 
 var height=$('#viewport').height();
 var width=$('#viewport').width();
@@ -58,14 +74,6 @@ $pages.swipeDown(function(){
 
 
 
-
-// $('body').tap(function(){
-//    return false;
-// })
-
-// $('body').tap(function(){
-//    return false;
-// })
 
 
 
@@ -125,35 +133,67 @@ $pages.swipeDown(function(){
 
 
 
-var n=$('body').get(0);
+// var n=$('body').get(0);
 
-
-
-var f;
-n.addEventListener("touchstart", function(e) {         //n为h5页面的body
-                f = e.touches[0].clientY;
-
-},!1),
-n.addEventListener("touchmove", function(t) {
-  var i = "11"
-  , n = this;
-  if (0 === n.scrollTop)                                  //滚动条最顶部的情况。
-       // 如果说没有滚动条的情况，i = "00"; 有滚动条的情况为“01”。
-       i = n.offsetHeight === n.scrollHeight ? "00" : "01";     
-  else if (n.scrollTop + n.offsetHeight >= n.scrollHeight)//有滚动条，滚动到最底部的情况。
-       i = "10";                 
- if ("11" !== i) {
-       //01是向上滑动或者滑不动，10是向下滑动。
-       var r = t.touches[0].clientY - f > 0 ? "10" : "01";
-       if (!(parseInt(i, 2) & parseInt(r, 2)))
-                        //e(t)
-   
-         
-          　t.stopPropagation();
-　　　　　　t.preventDefault();
+document.ontouchmove=function(e){
+	var e=event;
+	e.stopPropagation();
+    e.preventDefault();
 }
+
+
+
+
+
+
+// var f;
+// n.addEventListener("touchstart", function(e) {         //n为h5页面的body
+//                 f = e.touches[0].clientY;
+
+// },false);
+// n.addEventListener("touchmove", function(t) {
+//   var i = "11";
+//   if (0 === n.scrollTop)                                  //滚动条最顶部的情况。
+//        // 如果说没有滚动条的情况，i = "00"; 有滚动条的情况为“01”。
+//        i = n.offsetHeight === n.scrollHeight ? "00" : "01";     
+//   else if (n.scrollTop + n.offsetHeight >= n.scrollHeight)//有滚动条，滚动到最底部的情况。
+//        i = "10";                 
+//  if ("11" !== i) {
+//        //01是向上滑动或者滑不动，10是向下滑动。
+//        var r = t.touches[0].clientY - f > 0 ? "10" : "01";
+//        if (!(parseInt(i, 2) & parseInt(r, 2)))
      
-  },!1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+//           　t.stopPropagation();
+// 　　　　　　t.preventDefault();
+// // }
+     
+  // },false);
 
 
 
