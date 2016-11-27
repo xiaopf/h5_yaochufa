b09eeb2508a15eddadae788af09701b22e238f90
xiaopf <326258842@qq.com>
2016-11-27 11:28:42 +0800

    as

--- script/script.js
+++ script/script.js
@@ -59,13 +59,13 @@ $pages.swipeDown(function(){
 
 
 
-$('body').tap(function(){
-   return false;
-})
+// $('body').tap(function(){
+//    return false;
+// })
 
-$('body').tap(function(){
-   return false;
-})
+// $('body').tap(function(){
+//    return false;
+// })
 
 
 
@@ -119,47 +119,41 @@ $('body').tap(function(){
 
 //  }
 
-
-
-
-
-
-
-
-
 //   },false);
 
 
 
-// var n=$('body').get(0);
+
+
+var n=$('body').get(0);
 
 
 
-// var f;
-// n.addEventListener("touchstart", function(e) {         //n为h5页面的body
-//                 f = e.touches[0].clientY;
+var f;
+n.addEventListener("touchstart", function(e) {         //n为h5页面的body
+                f = e.touches[0].clientY;
 
-// },!1),
-// n.addEventListener("touchmove", function(t) {
- //  var i = "11"
- //  , n = this;
- //  if (0 === n.scrollTop)                                  //滚动条最顶部的情况。
- //       // 如果说没有滚动条的情况，i = "00"; 有滚动条的情况为“01”。
- //       i = n.offsetHeight === n.scrollHeight ? "00" : "01";     
- //  else if (n.scrollTop + n.offsetHeight >= n.scrollHeight)//有滚动条，滚动到最底部的情况。
- //       i = "10";                 
- // if ("11" !== i) {
- //       //01是向上滑动或者滑不动，10是向下滑动。
- //       var r = t.touches[0].clientY - f > 0 ? "10" : "01";
- //       if (!(parseInt(i, 2) & parseInt(r, 2)))
- //                        //e(t)
-   // alert(n.scrollTop)
-//           if(n.scrollTop!=0){
-//           　t.stopPropagation();
-// 　　　　　　t.preventDefault();
-// }
-     // }
-//   },!1);
+},!1),
+n.addEventListener("touchmove", function(t) {
+  var i = "11"
+  , n = this;
+  if (0 === n.scrollTop)                                  //滚动条最顶部的情况。
+       // 如果说没有滚动条的情况，i = "00"; 有滚动条的情况为“01”。
+       i = n.offsetHeight === n.scrollHeight ? "00" : "01";     
+  else if (n.scrollTop + n.offsetHeight >= n.scrollHeight)//有滚动条，滚动到最底部的情况。
+       i = "10";                 
+ if ("11" !== i) {
+       //01是向上滑动或者滑不动，10是向下滑动。
+       var r = t.touches[0].clientY - f > 0 ? "10" : "01";
+       if (!(parseInt(i, 2) & parseInt(r, 2)))
+                        //e(t)
+   alert(n.scrollTop)
+          if(n.scrollTop!=0){
+          　t.stopPropagation();
+　　　　　　t.preventDefault();
+}
+     }
+  },!1);
 
 
 
